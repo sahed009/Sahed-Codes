46393f5966be2a8865e7533a88fdb6c9da515a99
sahed009 <saheduzzamansobuj@gmail.com>
2024-06-15 10:37:32 +0600

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -6,24 +6,43 @@ import Expertise from "./pages/expertise";
 import Projects from "./pages/projects";
 import Testimonials from "./pages/testimonials";
 import { Routes, Route } from "react-router";
+import HashLoader from "react-spinners/HashLoader";
+import "./App.css"
 
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
+	const [loading, setloading] = useState(false);
+
 	useEffect(() => {
-		Aos.init({ duration:800, easing: "linear"});
+		setloading(true)
+		setTimeout(() => {
+			setloading(false)
+		}, 2500)
+	}, [])
+
+
+	useEffect(() => {
+		Aos.init({ duration: 800, easing: "linear" });
 	}, []);
 	return (
 		<>
-			<Routes>
+			{loading ? <div className="fixed flex h-vh w-full justify-center items-center top-1/2">
+				<HashLoader
+					color="#50C878"
+					size={100}
+					cssOverride={{}}
+				/>
+			</div> : <Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="/expertise" element={<Expertise />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/testimonials" element={<Testimonials />} />
-			</Routes>
+			</Routes>}
+
 		</>
 	);
 };
