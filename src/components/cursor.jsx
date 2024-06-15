80fb0b09d7716be1e0e46f6ef99adc97a1d63839
sahed009 <saheduzzamansobuj@gmail.com>
2024-06-16 01:09:37 +0600

    cursor on mobile fixed

--- src/components/cursor.jsx
+++ src/components/cursor.jsx
@@ -2,23 +2,37 @@ import React, { useEffect, useState } from 'react';
 
 const Cursor = () => {
     const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [showCursor, setShowCursor] = useState(false);
 
     useEffect(() => {
         const handleMouseMove = (event) => {
             setPosition({ x: event.clientX, y: event.clientY });
         };
 
+        const handleWindowResize = () => {
+            setShowCursor(window.innerWidth >= 1024);
+        };
+
+        handleWindowResize(); 
+
         window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('resize', handleWindowResize);
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('resize', handleWindowResize);
         };
     }, []);
 
+    if (!showCursor) return null;
+
+    
+    const cursorSize = 20; 
+
     return (
         <div
-            className="z-50 fixed top-0 left-0 w-10 h-10 border-[2px] border-[#53c87b] rounded-full pointer-events-none transform -translate-x-1/2 -translate-y-1/2 duration-200 ease-linear"
-            style={{ top: `${position.y}px`, left: `${position.x}px` }}
+            className="z-50 fixed top-0 left-0 w-10 h-10 border-[2px] border-[#53c87b] rounded-full pointer-events-none transform -translate-x-1/2 -translate-y-1/2 duration-75 ease-linear"
+            style={{ width: `${cursorSize}px`, height: `${cursorSize}px`, top: `${position.y}px`, left: `${position.x}px` }}
         />
     );
 };
--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CV = () => {
 	return (
-		<button className="bg-[#50C878] text-[#000000] min-w-[160px] font-semibold  min-h-[48px] shadow rounded-md hover:text-lg transition-all duration-300">
+		<button className="bg-[#50C878] text-[#000000] min-w-[140px] md:min-w-[160px] font-semibold  min-h-[48px] shadow rounded-md hover:text-lg transition-all duration-300">
 			<a href={require("../assets/My Resume.pdf")}>Download CV</a>
 		</button>
 	);
